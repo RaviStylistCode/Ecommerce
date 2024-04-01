c8b11c2e252dfe648d48f0379298baa6a340f5f1
Ravi Bhushan <ravibhu730@gmail.com>
2024-04-01 17:43:23 +0530

    pt sol

--- utils/multer.js
+++ utils/multer.js
@@ -1,10 +1,11 @@
 import multer from "multer";
 import {v4 as uuid} from "uuid"
-import path from "path";
+// import path from "path";
 
 const storage= multer.diskStorage({
     destination(req,file,callback){
-        callback(null,path.join(__dirname,'uploads'))
+        // callback(null,path.join(__dirname,'uploads'))
+        callback(null,'uploads')
     },
     filename(req,file,callback){
         const id=uuid();
